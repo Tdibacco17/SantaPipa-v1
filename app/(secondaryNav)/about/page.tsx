7ab1ae72054da1c74c4ed5f2dab09f7ef3e573f7
Tdibacc17 <tomasdibacco@gmail.com>
2023-11-24 10:25:43 -0300

    feature | about page

--- app/(secondaryNav)/about/page.tsx
+++ app/(secondaryNav)/about/page.tsx
@@ -1,14 +1,29 @@
+import { ImgDataInterface } from '@/types'
 import styles from './page.module.scss'
-
+import data from '@/models/es.json'
+import Image from 'next/image'
 export default function CampaignPage() {
     return (
         <div className={styles["container-section-about"]}>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <p>Página de Nosotros</p>
+            <div className={styles["wrapper"]}>
+                <div className={styles["container-header"]}>
+                    <p className={styles["title"]}>{data.aboutPage.title}</p>
+                    <small className={styles["subtitle"]}>{data.aboutPage.subtitle}</small>
+                </div>
+                <div className={styles["grid"]}>
+                    {data.aboutPage.images.map((imageData: ImgDataInterface, index: number) => {
+                        return <div key={index} className={styles["container-outer-img"]}>
+                            <Image
+                                className={styles["container-inner-img"]}
+                                src={imageData.imgSrc}
+                                alt={imageData.imgAlt}
+                                fill
+                                style={{ objectPosition: imageData.objPosition }}
+                            />
+                        </div>
+                    })}
+                    d</div>
+            </div>
         </div>
     )
 }
