cc7a278e1ba46459914d9bd80d09b448acf86251
Tdibacc17 <tomasdibacco@gmail.com>
2023-11-24 10:30:18 -0300

    fix | position text

--- app/(secondaryNav)/about/page.tsx
+++ app/(secondaryNav)/about/page.tsx
@@ -7,7 +7,11 @@ export default function CampaignPage() {
         <div className={styles["container-section-about"]}>
             <div className={styles["wrapper"]}>
                 <div className={styles["container-header"]}>
-                    <p className={styles["title"]}>{data.aboutPage.title}</p>
+                    <p className={styles["title"]}>
+                        {data.aboutPage.title.map((item: string, index: number) => {
+                            return <span key={index}>{item}</span>
+                        })}
+                    </p>
                     <small className={styles["subtitle"]}>{data.aboutPage.subtitle}</small>
                 </div>
                 <div className={styles["grid"]}>
