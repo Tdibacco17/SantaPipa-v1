1cd364e2acdd5f8013d8cee862dc3b1dc6374c82
Tdibacc17 <tomasdibacco@gmail.com>
2023-11-24 10:26:12 -0300

    fix | letter 'd'

--- app/(secondaryNav)/about/page.tsx
+++ app/(secondaryNav)/about/page.tsx
@@ -22,7 +22,7 @@ export default function CampaignPage() {
                             />
                         </div>
                     })}
-                    d</div>
+                </div>
             </div>
         </div>
     )
