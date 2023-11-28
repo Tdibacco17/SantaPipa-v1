2391fdb76fe6fd98f84f7e1067bf93605cc96299
Tdibacc17 <tomasdibacco@gmail.com>
2023-11-28 11:32:11 -0300

    user-scalable

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: 'SANTA PIPA',
   description: 'Tienda de ropa',
   applicationName: 'Santa Pipa',
-  viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+  viewport: 'width=device-width, initial-scale=1',
   colorScheme: 'light',
   themeColor: '#FFFFFF',
   authors: {
